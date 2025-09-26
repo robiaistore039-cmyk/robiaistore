2895baeb2a13bdbe2a304d10c9c519785d94c8fd
robiaistore039-cmyk <robiaistore039@gmail.com>
2025-09-26 17:22:37 +0600

    All files added

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple script for mobile menu toggle
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
+
